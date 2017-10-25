2b60f1bba5a151188c705e51e38c99ddf7621ed3
gregxrenner <gregxrenner@gmail.com>
2017-10-25 09:24:44 -0600

    Made a function to map a location data to a US map and explored the Baltimore market.

--- Real Estate Market Analysis/Home Analysis.R
+++ Real Estate Market Analysis/Home Analysis.R
@@ -8,11 +8,50 @@
 # and 'ZRI Summary: Multifamily, SFR, Condo/Co-op (Current Month)'.
 
 #load home rents
-rents <- read.csv2('C:/Users/Gregory.Renner/Documents/Personal/Programs/Data-Analysis/Real Estate Market Analysis/Zip_Zhvi_Summary_AllHomes.csv', header = TRUE, sep=",")
+rentPath <- '/home/greg/Documents/Projects/Data-Analysis/Real Estate Market Analysis/Zip_Zri_AllHomesPlusMultifamily_Summary.csv'
+rents <- read.csv2(rentPath, header = TRUE, sep=",")
 head(rents, n=5)
+#ZRI's above $4000 are likely errors so drop any locations with ZRI's above $4k
 
 #load home values
-values <- read.csv2('C:/Users/Gregory.Renner/Documents/Personal/Programs/Data-Analysis/Real Estate Market Analysis/Zip_Zri_AllHomesPlusMultifamily_Summary.csv', header = TRUE, sep=",")
+valuePath <- '/home/greg/Documents/Projects/Data-Analysis/Real Estate Market Analysis/Zip_Zhvi_Summary_AllHomes.csv'
+values <- read.csv2(valuePath, header = TRUE, sep=",")
 head(values, n=5)
 
+#create a single dataset, remove redundent variables before doing so
+rents <- rents[c("RegionName", "SizeRank", "Zri", "MoM", "QoQ", "YoY", "ZriRecordCnt")]
+homeData <- merge(rents, values, by = "RegionName")
+colnames(homeData)[colnames(homeData)=="RegionName"] <- "ZipCode"
 
+#for a first cut, look at markets with high median rents compared to median values
+homeData$heatIndex <- homeData$Zri/homeData$Zhvi
+hist(homeData$heatIndex)
+
+#install zipcode package before continuing
+library(zipcode)
+library(ggplot2)
+data(zipcode)
+colnames(zipcode)[colnames(zipcode)=="zip"] <- "ZipCode"
+homeData <- merge(homeData, zipcode, by.x='ZipCode', by.y='ZipCode')
+homeData$region <- substr(homeData$ZipCode, 1, 1)
+
+plotUS <- function(data, color){
+  g <- ggplot(data=data) + geom_point(aes(x=longitude, y=latitude, colour=(color))) + scale_colour_gradientn(colours=rev(rainbow(4)))
+  
+  # simplify display and limit to the "lower 48"
+  g = g + theme_bw() + scale_x_continuous(limits = c(-125,-66), breaks = NULL)
+  g = g + scale_y_continuous(limits = c(25,50), breaks = NULL)
+  
+  # don't need axis labels
+  g = g + labs(x=NULL, y=NULL)
+  
+  print(g)
+}
+
+plotUS(homeData, homeData$heatIndex)
+
+#sort the dataset by heat index to look for the best potential markets
+homeData <- homeData[order(homeData$heatIndex, decreasing = TRUE), ]
+
+#Baltimore and Philly look hot
+Baltimore <- homeData[which(homeData$City == "Baltimore"), ]
