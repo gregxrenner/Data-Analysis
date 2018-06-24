63ccc5b175ae19f7388e2900ea225a046faa1c63
gregxrenner <gregxrenner@gmail.com>
2018-06-24 15:28:14 -0600

    Completed coding challenge and Pandas dataviz exercise

--- Coding Challenges/ZigZag.py
+++ Coding Challenges/ZigZag.py
@@ -1,13 +1,64 @@
-# A sequence of integers is called a zigzag sequence if
-# each of its elements is either strictly less than all
-# its neighbors or strictly greater than all its neighbors.
-# For example, the sequence 4 2 3 1 5 3 is a zigzag,
-# but 7 3 5 5 2 and 3 8 6 4 5 aren't. Sequence of length 1
-# is also a zigzag.
+def zigZag(a):
+    # initialize zigzag as empty list
+    longest_zz = [a[0]]
+    zigzag = []
+    zigging = False
+    seq = []
+    index = 0
+    # check if the first character is part of a zigZag
+    if (a[0] < a[1] and a[1] > a[2]) or (a[0] > a[1] and a[1] < a[2]):
+        seq = [a[0]] # add the first character to zigzag
+        zigging = True
+    else:
+        zigging = False
+    # itierate through the list
+    for i in a[1:-1]:
+        index += 1
+        print("i=", i)
+        if a[index-1] < i and a[index+1] < i:
+            if zigging == True:
+                seq.append(i)
+            else:
+                seq = [a[index-1], i]
+                zigging = True
+        elif a[index-1] > i and a[index+1] > i:
+            if zigging == True:
+                seq.append(i)
+            else:
+                seq = [a[index-1], i]
+                zigging = True
+        else:
+            if zigging == True:
+                seq.append(i)
+                zigzag = list(seq)
+                zigging = False
+            seq = []
+        if len(zigzag) >= len(longest_zz):
+            longest_zz = list(zigzag)
+        print("seq=", seq)
+        print("zigzag=", zigzag)
+        print("longest_zz=", longest_zz)
+    # check is list ends while zigzaggin
+    if zigging == True:
+        print('List ends while zigging.')
+        seq.append(a[-1])
+        zigzag = list(seq)
+        print("seq=", seq)
+        print("zigzag=", zigzag)
+        if len(zigzag) >= len(longest_zz):
+            longest_zz = list(zigzag)
+        print("longest_zz=", longest_zz)
+    # Handle case where no zigzag is found to be longer than 1
+    if len(longest_zz)==1:
+        if (a[0] != a[1]):
+            longest_zz = a[0:2]
+        elif (a[-2] != a[-1]):
+            longest_zz = a[-2:]
+    return len(longest_zz)
 
-# For a given array of integers return the length of its
-# longest contiguous sub-array that is a zigzag sequence.
 
-def ZigZag(a):
-    # Assume 2 < a.length < 25 and 0 < a[i] < 100
-    
+test = [9, 8, 8, 5, 3, 5, 3, 2, 8, 6]
+test = [2, 1, 4, 4, 1, 4, 4, 1, 2, 0, 1, 0, 0, 3, 1, 3, 4, 1, 3, 4]
+test = [1, 3, 2, 3, 4, 5, 1, 4, 2, 3]
+test = [1, 2, 2, 2]
+zigZag(test)
